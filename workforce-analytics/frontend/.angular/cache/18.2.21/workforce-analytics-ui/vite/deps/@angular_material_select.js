6e5b9aaec425649c237c8ab91c9277d2cced8c22
Tariq Ahsan <tariqahsan@yahoo.com>
2026-05-05 22:49:49 -0400

    added missing files

---
+++ workforce-analytics/frontend/.angular/cache/18.2.21/workforce-analytics-ui/vite/deps/@angular_material_select.js
@@ -0,0 +1,55 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-T5T2XEQQ.js";
+import "./chunk-AHWLEG4P.js";
+import "./chunk-O2TTR2P2.js";
+import "./chunk-XUPB2GYR.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-KVUFRPBG.js";
+import "./chunk-GZCVTFTR.js";
+import "./chunk-JYQKWHEH.js";
+import {
+  MatOptgroup,
+  MatOption
+} from "./chunk-6ZLJHP2R.js";
+import "./chunk-W2BAPFRP.js";
+import "./chunk-TL4DGONE.js";
+import "./chunk-FFZIAYYX.js";
+import "./chunk-6Q4RANH6.js";
+import "./chunk-CXCX2JKZ.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
